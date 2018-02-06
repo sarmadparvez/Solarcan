c5e74290b5f0dba445d0cbdcac7f79d79c0e3338
Khubaib <khubaib.afzal@rolustech.com>
2018-02-06 19:19:04 +0500

    Packages

--- Packages/Telemarketers/custom/Extension/modules/tm_telemarketers/Ext/LogicHooks/tm_telemarketersHooks.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $hook_array['before_save'][] = array(
-        1,
-        'Throw exception if noagent is duplicate',
-        'custom/modules/tm_telemarketers/tm_telemarketersHooksImpl.php',
-        'tm_telemarketersHooksImpl',
-        'beforeSave'
-    );
