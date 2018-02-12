6bed71cef98f7e487304bc246eda42cf4c8fadb4
sarmadparvez <sarmad.pervaiz@rolustech.com>
2018-02-12 14:37:03 +0500

    Redmine #63247: Classiciations Module

---
+++ Packages/rt_classification2018_01_31_114716/SugarModules/modules/rt_Classification/language/ar_SA.lang.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+$mod_strings = array (
+  'LBL_TEAM' => 'الفرق',
+  'LBL_TEAMS' => 'الفرق',
+  'LBL_TEAM_ID' => 'معرّف الفريق',
+  'LBL_ASSIGNED_TO_ID' => 'معرّف المستخدم المعين',
+  'LBL_ASSIGNED_TO_NAME' => 'تعيين إلى',
+  'LBL_TAGS_LINK' => 'العلامات',
+  'LBL_TAGS' => 'العلامات',
+  'LBL_ID' => 'المعرّف',
+  'LBL_DATE_ENTERED' => 'تاريخ الإنشاء',
+  'LBL_DATE_MODIFIED' => 'تاريخ التعديل',
+  'LBL_MODIFIED' => 'تم التعديل بواسطة',
+  'LBL_MODIFIED_ID' => 'تم التعديل بواسطة المعرّف',
+  'LBL_MODIFIED_NAME' => 'تم التعديل بواسطة الاسم',
+  'LBL_CREATED' => 'تم الإنشاء بواسطة',
+  'LBL_CREATED_ID' => 'تم الإنشاء بواسطة المعرّف',
+  'LBL_DOC_OWNER' => 'مالك المستند',
+  'LBL_USER_FAVORITES' => 'المستخدمون الذي يفضلون',
+  'LBL_DESCRIPTION' => 'الوصف',
+  'LBL_DELETED' => 'تم الحذف',
+  'LBL_NAME' => 'الاسم',
+  'LBL_CREATED_USER' => 'تم الإنشاء بواسطة مستخدم',
+  'LBL_MODIFIED_USER' => 'تم التعديل بواسطة مستخدم',
+  'LBL_LIST_NAME' => 'الاسم',
+  'LBL_EDIT_BUTTON' => 'تحرير',
+  'LBL_REMOVE' => 'إزالة',
+  'LBL_EXPORT_MODIFIED_BY_NAME' => 'تم التعديل بواسطة الاسم',
+  'LBL_LIST_FORM_TITLE' => 'Classification القائمة',
+  'LBL_MODULE_NAME' => 'Classification',
+  'LBL_MODULE_TITLE' => 'Classification',
+  'LBL_MODULE_NAME_SINGULAR' => 'Classification',
+  'LBL_HOMEPAGE_TITLE' => 'الخاص بي Classification',
+  'LNK_NEW_RECORD' => 'إنشاء Classification',
+  'LNK_LIST' => 'عرض Classification',
+  'LNK_IMPORT_RT_CLASSIFICATION' => 'Import Classification',
+  'LBL_SEARCH_FORM_TITLE' => 'بحث Classification',
+  'LBL_HISTORY_SUBPANEL_TITLE' => 'عرض السجل',
+  'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'سير النشاط الكلي',
+  'LBL_RT_CLASSIFICATION_SUBPANEL_TITLE' => 'Classification',
+  'LBL_NEW_FORM_TITLE' => 'جديد Classification',
+  'LNK_IMPORT_VCARD' => 'Import Classification vCard',
+  'LBL_IMPORT' => 'Import Classification',
+  'LBL_IMPORT_VCARDTEXT' => 'Automatically create a new Classification record by importing a vCard from your file system.',
+  'LBL_EFFICIENCY' => 'Efficiency',
+  'LBL_MIN_APPOINTMENT_PER_WEEK' => 'Min no of appointments per week',
+  'LBL_MAX_APPOINTMENT_PER_WEEK' => 'Max no of appointments per week',
+  'LBL_APPOINTMENT_PER_DAY' => 'No of appointments per day',
+  'LBL_SURPLUS_APPOINTMENT_COUNT' => 'No of appointments when surplus',
+);
---
+++ Packages/rt_classification2018_01_31_114716/SugarModules/modules/rt_Classification/language/fi_FI.lang.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * Your installation or use of this SugarCRM file is subject to the applicable
+ * terms available at
+ * http://support.sugarcrm.com/Resources/Master_Subscription_Agreements/.
+ * If you do not agree to all of the applicable terms or do not have the
+ * authority to bind the entity as an authorized representative, then do not
+ * install or use this SugarCRM file.
+ *
+ * Copyright (C) SugarCRM Inc. All rights reserved.
+ */
+$mod_strings = array (
+  'LBL_TEAM' => 'Tiimit',
+  'LBL_TEAMS' => 'Tiimit',
+  'LBL_TEAM_ID' => 'Tiimin tunnus',
+  'LBL_ASSIGNED_TO_ID' => 'Vastuukäyttäjän ID',
+  'LBL_ASSIGNED_TO_NAME' => 'Vastuuhenkilö',
+  'LBL_TAGS_LINK' => 'Tagit',
+  'LBL_TAGS' => 'Tagit',
+  'LBL_ID' => 'ID',
+  'LBL_DATE_ENTERED' => 'Luontipäivä',
+  'LBL_DATE_MODIFIED' => 'Muokkattu viimeksi',
+  'LBL_MODIFIED' => 'Muokannut',
+  'LBL_MODIFIED_ID' => 'Muokkaajan ID',
+  'LBL_MODIFIED_NAME' => 'Muokkaajan nimi',
+  'LBL_CREATED' => 'Luonut',
+  'LBL_CREATED_ID' => 'Luojan ID',
+  'LBL_DOC_OWNER' => 'Tiedoston omistaja',
+  'LBL_USER_FAVORITES' => 'Käyttäjät, jotka ottivat suosikikseen',
+  'LBL_DESCRIPTION' => 'Kuvaus',
+  'LBL_DELETED' => 'Poistettu',
+  'LBL_NAME' => 'Nimi',
+  'LBL_CREATED_USER' => 'Luoja',
+  'LBL_MODIFIED_USER' => 'Muokkaaja',
+  'LBL_LIST_NAME' => 'Nimi',
+  'LBL_EDIT_BUTTON' => 'Muokkaa',
+  'LBL_REMOVE' => 'Poista',
+  'LBL_EXPORT_MODIFIED_BY_NAME' => 'Muokkaajan nimi',
+  'LBL_LIST_FORM_TITLE' => 'Classification Lista',
+  'LBL_MODULE_NAME' => 'Classification',
+  'LBL_MODULE_TITLE' => 'Classification',
+  'LBL_MODULE_NAME_SINGULAR' => 'Classification',
+  'LBL_HOMEPAGE_TITLE' => 'Oma Classification',
+  'LNK_NEW_RECORD' => 'Luo Classification',
+  'LNK_LIST' => 'Näkymä Classification',
+  'LNK_IMPORT_RT_CLASSIFICATION' => 'Import Classification',
+  'LBL_SEARCH_FORM_TITLE' => 'Etsi Classification',
+  'LBL_HISTORY_SUBPANEL_TITLE' => 'Näytä historia',
+  'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Aktiviteetit',
+  'LBL_RT_CLASSIFICATION_SUBPANEL_TITLE' => 'Classification',
+  'LBL_NEW_FORM_TITLE' => 'Uusi Classification',
+  'LNK_IMPORT_VCARD' => 'Import Classification vCard',
+  'LBL_IMPORT' => 'Import Classification',
+  'LBL_IMPORT_VCARDTEXT' => 'Automatically create a new Classification record by importing a vCard from your file system.',
+  'LBL_EFFICIENCY' => 'Efficiency',
+  'LBL_MIN_APPOINTMENT_PER_WEEK' => 'Min no of appointments per week',
+  'LBL_MAX_APPOINTMENT_PER_WEEK' => 'Max no of appointments per week',
+  'LBL_APPOINTMENT_PER_DAY' => 'No of appointments per day',
+  'LBL_SURPLUS_APPOINTMENT_COUNT' => 'No of appointments when surplus',
+);
