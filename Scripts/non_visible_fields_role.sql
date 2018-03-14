5f3c30a0f675912f3e59e5b88b10732c3bfb684c
Khubaib <khubaib.afzal@rolustech.com>
2018-03-14 11:09:05 +0500

    Non Visible Fields Role for Applying ACL on Meetings

---
+++ Scripts/non_visible_fields_role.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: select * from acl_roles
+LIMIT 0, 50000
+
+-- Date: 2018-03-14 11:06
+*/
+REPLACE INTO `acl_roles` (`id`,`date_entered`,`date_modified`,`modified_user_id`,`created_by`,`name`,`description`,`deleted`) VALUES ('06fd180a-25de-11e8-bbe7-2c56dc94b8c8','2018-03-12 10:13:36','2018-03-13 12:52:12','1','1','Non Visible Fields',NULL,0);
